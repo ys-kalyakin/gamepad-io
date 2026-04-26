feb71bf4085bfb809ab0cfbb4a7b0f05844f07d7
ys-kalyakin <ys.kalyakin@gmail.com>
2026-04-27 01:35:39 +0300

    feat: export IButtonConfig and IStickConfig for helper tool compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Gamepad, GamepadEvent } from './Gamepad';
 import { ILogger } from './ILogger';
-import { IConfig } from './IConfig';
-import { GamepadButton } from './IConfig';
+import { IConfig, IButtonConfig, IStickConfig, GamepadButton } from './IConfig';
 import { Combination } from './ICombination';
 import { StickValue } from './StickValue';
 import { gamepadConfigs, GamepadConfigName } from './gamepads';
@@ -9,7 +8,7 @@ import { gamepadConfigs, GamepadConfigName } from './gamepads';
 export { Gamepad };
 export { GamepadEvent };
 export { ILogger };
-export { IConfig };
+export { IConfig, IButtonConfig, IStickConfig };
 export { GamepadButton };
 export { Combination };
 export { StickValue };
