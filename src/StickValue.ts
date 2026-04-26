e2d5404b8475611296019fdf847208db216e53ad
ys-kalyakin <ys.kalyakin@gmail.com>
2026-04-26 19:29:46 +0300

    docs: add JSDoc for StickValue interface

--- src/StickValue.ts
+++ src/StickValue.ts
@@ -1,4 +1,13 @@
+/**
+ * Represents a joystick/stick position value.
+ *
+ * Values typically range from -1.0 to 1.0, where (0, 0) represents the neutral/center position.
+ * X axis: -1.0 = left, 1.0 = right
+ * Y axis: -1.0 = up, 1.0 = down
+ */
 export interface StickValue {
+    /** X-axis position (-1.0 to 1.0) */
     x: number;
+    /** Y-axis position (-1.0 to 1.0) */
     y: number;
 }
